0ea5f6f469f6b330ab2db738721ec83a47d3142e
vladich <vladichnaprimer@gmail.com>
2016-11-11 10:03:16 +0200

    Initial commit

---
+++ app/js/controllers.js
@@ -0,0 +1,9 @@
+var phonecatApp = angular.module('phonecatApp', []);
+
+phonecatApp.controller('PhoneListCtrl',['$scope','$http', function($scope, $http) {
+
+    $http.get('phones/phones.json').success(function(data, status, headers, config) {
+        $scope.phones = data;
+    });
+
+}]);
